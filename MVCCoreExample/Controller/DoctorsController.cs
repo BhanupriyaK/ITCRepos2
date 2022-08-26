b9f2e8c2ccce2f7ec5fcfdb3c6aa4858b8d4ffbc
BhanupriyaK <112075332+BhanupriyaK@users.noreply.github.com>
2022-08-26 14:48:47 +0530

    first commit

---
+++ MVCCoreExample/Controller/DoctorsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCCoreExample.Models;
+
+namespace MVCCoreExample.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        public IActionResult Index()
+
+        {
+            List<Doctor> dlist = new List<Doctor>();
+            Doctor d1 = new Doctor();
+            {
+                
+
+  
+
+            
+              
+            
+
+            }
+            return View(dlist);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit() 
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCCoreExample/Models/Doctor.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCCoreExample.Models
+{
+    public class Doctor
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public string speciality { get; set; }
+    }
+}
